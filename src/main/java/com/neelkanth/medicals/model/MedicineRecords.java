4689976eca37d014fd90cf9f369f6bc1ed67cba8
ankitdungarwal06 <ankitdgeek@gmail.com>
2023-07-07 08:54:01 +0530

    adding additional feilds for future purposes

--- src/main/java/com/neelkanth/medicals/model/MedicineRecords.java
+++ src/main/java/com/neelkanth/medicals/model/MedicineRecords.java
@@ -33,6 +33,19 @@ public class MedicineRecords {
 	private float priceOneSachet;
 	private String remarks;
 	private String[] comments;
+	private String col1;
+	private String col2;
+	private String col3;
+	private String col4;
+	private String col5;
+	private String col6;
+	private String col7;
+	private String col8;
+	private String col9;
+	private String col10;
+	private String col11;
+	private String col12;
+	
 }
 
 enum MedicineType{
